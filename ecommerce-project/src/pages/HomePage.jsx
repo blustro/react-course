cae223b4ddad7276ffbb85d2da5b5e05cfa6e6ee
Bruno Lustro <brunolustro@gmail.com>
2026-03-05 17:26:19 -0300

    Use axiso to fetch data

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -1,9 +1,14 @@
+import axios from 'axios';
 import { Header } from '../components/Header';
 import { products } from '../../starting-code/data/products';
 
 import './HomePage.css';
 
 export const HomePage = () => {
+  axios.get('http://localhost:3000/api/products').then((response) => {
+    console.log(response.data);
+  });
+
   return (
     <>
       <title>Ecommerce Project</title>
