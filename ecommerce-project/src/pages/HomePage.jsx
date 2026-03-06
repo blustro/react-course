c03df2e6c7674fa942794232b74c44ba2bed7d12
Bruno Lustro <brunolustro@gmail.com>
2026-03-05 21:01:00 -0300

    Add server proxy config for /api and /images

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -5,10 +5,15 @@ import './HomePage.css';
 
 export const HomePage = () => {
   const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState([]);
 
   useEffect(() => {
-    axios.get('http://localhost:3000/api/products').then((response) => {
+    axios.get('/api/products').then((response) => {
       setProducts(response.data);
+    });
+
+    axios.get('/api/cart-items').then((response) => {
+      setCart(response.data);
     });
   }, []);
 
@@ -16,7 +21,7 @@ export const HomePage = () => {
     <>
       <title>Ecommerce Project</title>
 
-      <Header />
+      <Header cart={cart} />
 
       <div className='home-page'>
         <div className='products-grid'>
