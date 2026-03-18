b00cd97d2adc3ee5259dbfe65541f6dde9cce40c
Bruno Lustro <brunolustro@gmail.com>
2026-03-17 21:28:44 -0300

    Add HomePage integration test

--- ecommerce-project/src/pages/home/Product.jsx
+++ ecommerce-project/src/pages/home/Product.jsx
@@ -26,7 +26,7 @@ export const Product = ({ product, loadCart }) => {
   };
 
   return (
-    <div className='product-container'>
+    <div className='product-container' data-testid='product-container'>
       <div className='product-image-container'>
         <img
           className='product-image'
