ffc31b656ebdddd19ce00aed682bcdeca240ddf6
Bruno Lustro <brunolustro@gmail.com>
2026-05-05 14:39:01 -0300

    add tailwind styles to Header

--- ecommerce-project/src/components/Header.jsx
+++ ecommerce-project/src/components/Header.jsx
@@ -25,33 +25,45 @@ export const Header = () => {
 
   return (
     <>
-      <div className='header'>
-        <div className='left-section'>
-          <Link to='/' className='header-link'>
-            <img className='logo' src='images/logo-white.png' />
-            <img className='mobile-logo' src='images/mobile-logo-white.png' />
+      <div className='bg-[rgb(8,79,45)] text-white px-3.75 flex items-center justify-between fixed top-0 left-0 right-0 h-15 z-50'>
+        <div className='w-52 max-[800px]:w-auto'>
+          <Link
+            to='/'
+            className='inline-block py-1.5 px-[9.5px] rounded-xs cursor-pointer no-underline border border-transparent hover:border-white transition-colors'
+          >
+            <img
+              className='h-6.5 mt-px block max-[675px]:hidden'
+              src='images/logo-white.png'
+            />
+            <img
+              className='h-6.5 mt-px hidden max-[675px]:block'
+              src='images/mobile-logo-white.png'
+            />
           </Link>
         </div>
 
-        <div className='middle-section'>
+        <div className='flex-1 max-w-212.5 mx-2.5 bg-white rounded-[5px]'>
           <form
-            className='search-form'
+            className='flex'
             onSubmit={(event) => {
               event.preventDefault();
               searchProducts();
             }}
           >
             <input
-              className='search-bar'
+              className='flex-1 w-0 text-[16px] h-9.5 pl-3.75 border-none rounded-l-[5px] rounded-r-none text-black'
               type='text'
               placeholder='Search'
               value={search}
               onChange={updateSearchInput}
             />
 
-            <button className='search-button' type='submit'>
+            <button
+              className='bg-[rgb(186,255,190)] border-none w-11.25 h-10 rounded-[5px] shrink-0 flex items-center justify-center'
+              type='submit'
+            >
               <img
-                className='search-icon'
+                className='h-5'
                 src='images/icons/search-icon.png'
                 alt='search'
               />
@@ -59,18 +71,30 @@ export const Header = () => {
           </form>
         </div>
 
-        <div className='right-section'>
-          <Link className='orders-link header-link' to='/orders'>
-            <span className='orders-text'>Orders</span>
+        <div className='w-45 shrink-0 flex justify-end'>
+          <Link
+            className='flex items-center px-3.25 text-white no-underline border border-transparent hover:border-white rounded-xs'
+            to='/orders'
+          >
+            <span className='block text-[15px] font-bold'>Orders</span>
           </Link>
 
-          <Link className='cart-link header-link' to='/checkout'>
-            <img className='cart-icon' src='images/icons/cart-icon.png' />
-            <div className='cart-quantity'>{totalQuantity}</div>
-            <div className='cart-text'>Cart</div>
+          <Link className='relative flex items-center ...' to='/checkout'>
+            <img
+              className='w-9.5'
+              src='images/icons/cart-icon.png'
+              alt='cart'
+            />
+
+            <div className='absolute top-0.5 right-1/2 w-6.5 text-center text-[rgb(8,79,45)] text-[14px] font-bold'>
+              {totalQuantity}
+            </div>
+
+            <div className='ml-1.25 text-[15px] font-bold'>Cart</div>
           </Link>
         </div>
       </div>
+      <div className='h-15'></div>
     </>
   );
 };
