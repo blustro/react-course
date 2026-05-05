77916e6078ce7c6c6a05836a061b4ea2b04d610b
Bruno Lustro <brunolustro@gmail.com>
2026-05-05 12:15:00 -0300

    remove loadCart prop

--- ecommerce-project/src/pages/home/ProductsGrid.jsx
+++ ecommerce-project/src/pages/home/ProductsGrid.jsx
@@ -1,12 +1,10 @@
 import { Product } from './Product';
 
-export const ProductsGrid = ({ products, loadCart }) => {
+export const ProductsGrid = ({ products }) => {
   return (
     <div className='products-grid'>
       {products.map((product) => {
-        return (
-          <Product key={product.id} product={product} loadCart={loadCart} />
-        );
+        return <Product key={product.id} product={product} />;
       })}
     </div>
   );
